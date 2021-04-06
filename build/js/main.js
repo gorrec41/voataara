1dbc2f71e8191a0e93f969345f361cb14e132853
vitaly <batrak1@ya.ru>
2021-04-07 00:05:51 +0300

    add foto

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tab = document.querySelectorAll('.tab__item'),
+        tabItems = document.querySelector('.tab__items'),
+        tabVideo = document.querySelectorAll('.tabcontent');
+    
+    function delActiv() {
+        tab.forEach(item => {
+            item.classList.remove('tab_active')
+            console.log(item)
+        })
+        tabVideo.forEach(item => {
+            item.classList.remove('activvideo')
+            console.log(item)
+        })
+    }
+    function addActiv(i=0) {
+        tab[i].classList.add('tab_active')
+        tabVideo[i].classList.add('activvideo')
+    }
+    tabItems.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && target.classList.contains('tab__item')) {
+            tab.forEach((item,i )=> {
+                if (target == item) {
+                    delActiv()
+                    addActiv(i)
+                }
+            })
+        }
+    })
 
+
+
+
+    
+})
